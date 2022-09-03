87ff594a67527fb2db97bab8de64d67cf4b6a80f
Charalampos Mitrodimas <charmitro@pm.me>
2022-09-04 01:47:39 +0300

    change: project name

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,7 @@
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
+pub struct ImOk {
     // Example stuff:
     label: String,
 
@@ -10,7 +10,7 @@ pub struct TemplateApp {
     value: f32,
 }
 
-impl Default for TemplateApp {
+impl Default for ImOk {
     fn default() -> Self {
         Self {
             // Example stuff:
@@ -20,7 +20,7 @@ impl Default for TemplateApp {
     }
 }
 
-impl TemplateApp {
+impl ImOk {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customized the look at feel of egui using
@@ -36,7 +36,7 @@ impl TemplateApp {
     }
 }
 
-impl eframe::App for TemplateApp {
+impl eframe::App for ImOk {
     /// Called by the frame work to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
