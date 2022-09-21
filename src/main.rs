29ed00d0a7d2c63b015bcb9c212727162306fc14
Charalampos Mitrodimas <charmitro@pm.me>
2022-09-21 07:33:39 +0300

    remove: trunk & wasm build

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 // When compiling natively:
-#[cfg(not(target_arch = "wasm32"))]
 fn main() {
 	// Log to stdout (if you run with `RUST_LOG=debug`).
 	use image::GenericImageView;
@@ -20,22 +19,4 @@ fn main() {
 		Some(eframe::IconData { rgba: icon.into_bytes(), width: icon_width, height: icon_height });
 
 	eframe::run_native("im ok", native_options, Box::new(|cc| Box::new(im_ok::ImOk::new(cc))));
-}
-
-// when compiling to web using trunk.
-#[cfg(target_arch = "wasm32")]
-fn main() {
-	// Make sure panics are logged using `console.error`.
-	console_error_panic_hook::set_once();
-
-	// Redirect tracing to console.log and friends:
-	tracing_wasm::set_as_global_default();
-
-	let web_options = eframe::WebOptions::default();
-	eframe::start_web(
-		"the_canvas_id", // hardcode it
-		web_options,
-		Box::new(|cc| Box::new(im_ok::ImOk::new(cc))),
-	)
-	.expect("failed to start eframe");
 }
