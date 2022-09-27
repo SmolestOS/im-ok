8ab6cbb6ad36b83261739359464533d141968a4f
Panagiotis Foliadis <pfoliadis@hotmail.com>
2022-09-28 01:07:56 +0300

    wip: error handling

--- api/src/controllers/mod.rs
+++ api/src/controllers/mod.rs
@@ -1,9 +1,21 @@
 pub mod nights;
 pub mod user;
 use crate::models::night::Night;
+use mongodb::bson::oid::ObjectId;
 
 #[derive(serde::Serialize, serde::Deserialize, Default)]
 pub struct Response {
 	pub msg: String,
 	pub data: Option<Night>,
+}
+#[derive(serde::Serialize, serde::Deserialize, Default)]
+pub struct ResponseNights {
+	pub msg: String,
+	pub data: Option<Vec<Night>>,
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Default)]
+pub struct DeletedId {
+	pub msg: String,
+	pub data: Option<ObjectId>,
 }
--- api/src/controllers/nights.rs
+++ api/src/controllers/nights.rs
@@ -8,9 +8,7 @@ use axum::{extract::Path, http::StatusCode, response::IntoResponse, Extension, J
 use futures::stream::TryStreamExt;
 use mongodb::bson::{oid::ObjectId, Bson};
 
-use super::Response;
-
-// TODO(@panosfol): get_night function;
+use super::{DeletedId, Response, ResponseNights};
 
 pub async fn get_one_night(
 	Path(params): Path<String>,
@@ -44,13 +42,17 @@ pub async fn get_one_night(
 	}
 }
 
-pub async fn get_all_nights(Extension(state): Extension<State>) -> impl IntoResponse {
+pub async fn get_all_nights(
+	Extension(state): Extension<State>,
+) -> (StatusCode, Json<ResponseNights>) {
+	let mut resp = ResponseNights::default();
 	let cursor = Night::get_all_nights(state.night_collection.clone()).await.unwrap();
 	let mut v: Vec<Night> = cursor.try_collect().await.unwrap();
 
-	// TODO(@panosfol): sort by date eg. `foo_items.sort_by(|a, b| a.date.cmp(&b.date));`
+	resp.msg = "Sucess".to_string();
 	v.sort_by(|a, b| a.craziness.date.cmp(&b.craziness.date));
-	(StatusCode::CREATED, Json(v))
+	resp.data = Some(v);
+	(StatusCode::CREATED, Json(resp))
 }
 
 pub async fn create_night(
@@ -69,13 +71,32 @@ pub async fn create_night(
 pub async fn delete_night(
 	Path(params): Path<String>,
 	Extension(state): Extension<State>,
-) -> impl IntoResponse {
-	let db_req =
-		Night::delete_night(state.night_collection, ObjectId::from_str(&params).unwrap()).await;
+) -> (StatusCode, Json<DeletedId>) {
+	let mut resp = DeletedId::default();
+	match ObjectId::from_str(&params) {
+		Ok(oid) => {
+			let db_req = Night::delete_night(state.night_collection, oid).await;
 
-	match db_req {
-		Ok(_) => (StatusCode::CREATED, Json(Bson::String("Success".to_string()))),
-		Err(err) => (StatusCode::BAD_REQUEST, Json(Bson::String(err.to_string()))),
+			match db_req {
+				Ok(res) =>
+					if let DeleteResult = res {
+						resp.data = Some(oid);
+						resp.msg = "Success".to_string();
+						(StatusCode::OK, Json(resp))
+					} else {
+						resp.msg = "Not found".to_string();
+						(StatusCode::BAD_REQUEST, Json(resp))
+					},
+				Err(err) => {
+					resp.msg = err.to_string();
+					(StatusCode::BAD_REQUEST, Json(resp))
+				},
+			}
+		},
+		Err(err) => {
+			resp.msg = err.to_string();
+			(StatusCode::BAD_REQUEST, Json(resp))
+		},
 	}
 }
 
@@ -83,13 +104,23 @@ pub async fn edit_night(
 	Path(params): Path<String>,
 	Json(payload): Json<Craziness>,
 	Extension(state): Extension<State>,
-) -> impl IntoResponse {
-	let db_req =
-		Night::edit_night(state.night_collection, ObjectId::from_str(&params).unwrap(), payload)
-			.await;
+) -> (StatusCode, Json<Response>) {
+	let mut resp = Response::default();
+	match ObjectId::from_str(&params) {
+		Ok(oid) => {
+			let db_req = Night::edit_night(state.night_collection, oid, payload).await;
 
-	match db_req {
-		Ok(_) => (StatusCode::CREATED, Json(Bson::String("Success".to_string()))),
-		Err(err) => (StatusCode::BAD_REQUEST, Json(Bson::String(err.to_string()))),
+			match db_req {
+				Ok(_) => (StatusCode::CREATED, Json(Bson::String("Success".to_string()))),
+				Err(err) => {
+					resp.msg = err.to_string();
+					(StatusCode::BAD_REQUEST, Json(resp))
+				},
+			}
+		},
+		Err(err) => {
+			resp.msg = err.to_string();
+			(StatusCode::BAD_REQUEST, Json(resp))
+		},
 	}
 }
