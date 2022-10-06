a9942c71b4b6c5d5055da268df10c678db1edb73
Panagiotis Foliadis <pfoliadis@hotmail.com>
2022-10-06 03:31:30 +0300

    add(api): User CRUD & create controller

--- api/src/controllers/user.rs
+++ api/src/controllers/user.rs
@@ -1,17 +1,32 @@
 use crate::{models::user::User, State};
-use axum::{http::StatusCode, response::IntoResponse, Extension, Json};
-use serde::Deserialize;
+use axum::{http::StatusCode, Extension, Json};
+use mongodb::bson::Bson;
 
-#[derive(Deserialize)]
-pub struct CreateUser {
-	username: String,
-	password: String,
+#[derive(serde::Serialize, serde::Deserialize, Default)]
+pub struct CreateResponse {
+	msg: String,
+	data: Option<Bson>,
 }
 pub async fn create_user(
-	Json(payload): Json<CreateUser>,
-	Extension(_state): Extension<State>,
-) -> impl IntoResponse {
-	let user = User { id: None, username: payload.username, password: payload.password };
+	Json(payload): Json<User>,
+	Extension(state): Extension<State>,
+) -> (StatusCode, Json<CreateResponse>) {
+	let mut resp = CreateResponse::default();
+	let db_req = User::create_night(
+		state.db_connection.collection::<User>("users"),
+		User { id: None, username: payload.username, password: payload.password },
+	)
+	.await;
 
-	(StatusCode::CREATED, Json(user))
+	match db_req {
+		Ok(res) => {
+			resp.msg = "Success".to_string();
+			resp.data = Some(res.inserted_id);
+			(StatusCode::OK, Json(resp))
+		},
+		Err(err) => {
+			resp.msg = err.to_string();
+			(StatusCode::BAD_REQUEST, Json(resp))
+		},
+	}
 }
--- api/src/models/user.rs
+++ api/src/models/user.rs
@@ -1,10 +1,19 @@
-use mongodb::bson::oid::ObjectId;
-use serde::Serialize;
+use mongodb::{bson::oid::ObjectId, error::Error, results::InsertOneResult};
+use serde::{Deserialize, Serialize};
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
 pub struct User {
 	#[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
 	pub id: Option<ObjectId>,
 	pub username: String,
 	pub password: String,
+}
+
+impl User {
+	pub async fn create_night(
+		collection: mongodb::Collection<User>,
+		item: User,
+	) -> std::result::Result<InsertOneResult, Error> {
+		collection.insert_one(item, None).await
+	}
 }
